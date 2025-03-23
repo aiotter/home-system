322e6b3837ebdac050c295f00ab352acc0980757
aiotter <git@aiotter.com>
2025-03-23 20:43:42 +0900

    refactor

--- flake.nix
+++ flake.nix
@@ -2,61 +2,52 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    aiotter-systems.url = "github:aiotter/systems/master";
-    aiotter-nixos.url = "github:aiotter/systems/nixos";
+    aiotter-systems.url = "github:aiotter/systems/nixos";
     colmena.url = "github:zhaofengli/colmena";
     colmena.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, aiotter-systems, aiotter-nixos, colmena }:
-    let
-      specialArgs = { inherit (aiotter-systems.lib) consts; };
-      modules = [
-        aiotter-nixos.nixosModules.raspi
+  outputs = { self, nixpkgs, nixos-hardware, aiotter-systems, colmena }: {
+    nixosModules.default = {
+      imports = [
+        aiotter-systems.nixosModules.raspi
         nixos-hardware.nixosModules.raspberry-pi-4
-        ./configuration.nix # local config
+        ./configuration.nix
         ./hardware-configuration.nix
       ];
-    in
-    {
-      nixosConfigurations = {
-        nixos = self.nixosConfigurations.home;
-        home = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          inherit modules specialArgs;
-        };
-      };
-
-      colmena = {
-        meta = {
-          nixpkgs = import nixpkgs { system = "aarch64-linux"; };
-          inherit specialArgs;
-        };
-        home = {
-          imports = modules;
-          deployment = {
-            targetHost = "home.local";
-            targetUser = "aiotter";
-            buildOnTarget = true;
-          };
-        };
-      };
-
-      colmenaHive = colmena.lib.makeHive self.outputs.colmena;
-
-      # --experimental-flake-eval を常時有効にした colmena
-      defaultApp =
-        let
-          makeApp = system:
-            let
-              pkgs = import nixpkgs { inherit system; };
-              colmenaPkgs = colmena.outputs.packages.${system};
-              colmena-wrapped = pkgs.writeShellScript "colmena" ''
-                ${colmenaPkgs.colmena}/bin/colmena --experimental-flake-eval "$@"
-              '';
-            in
-            { type = "app"; program = toString colmena-wrapped; };
-        in
-        builtins.mapAttrs (system: _: makeApp system) colmena.outputs.packages;
     };
+
+    nixosConfigurations.home = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      specialArgs = { inherit (aiotter-systems.lib) consts; };
+      modules = [ self.nixosModules.default ];
+    };
+
+    colmena = {
+      meta = {
+        nixpkgs = import nixpkgs { system = "aarch64-linux"; };
+        specialArgs = { inherit (aiotter-systems.lib) consts; };
+      };
+      home = {
+        inherit (self.nixosModules.default) imports;
+        deployment = {
+          targetHost = "home.local";
+          targetUser = "aiotter";
+          buildOnTarget = true;
+        };
+      };
+    };
+
+    colmenaHive = colmena.lib.makeHive self.outputs.colmena;
+
+    # --experimental-flake-eval を常時有効にした colmena
+    defaultPackage =
+      builtins.mapAttrs
+        (system: colmenaPkgs:
+          nixpkgs.legacyPackages.${system}.writeShellScriptBin "colmena" ''
+            ${colmenaPkgs.colmena}/bin/colmena --experimental-flake-eval "$@"
+          ''
+        )
+        colmena.outputs.packages;
+  };
 }
