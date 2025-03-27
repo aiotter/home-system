3385f6b4bb047bcec8da0ce9f68f400eb8578ac4
aiotter <git@aiotter.com>
2025-03-27 18:51:15 +0900

    feat(cloudflare-tunnel): Cloudflare Access での ssh ログインをより正しい形で設定
    ログイン者が誰であっても、資格があれば aiotter としてログイン可能にする

--- modules/cloudflare-tunnel.nix
+++ modules/cloudflare-tunnel.nix
@@ -1,7 +1,13 @@
 { pkgs, lib, config, ... }:
 
 let
-  public-key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGGucn5vE5q4Rk5ulsFVUAVSwQVbSEcwl0R7bxJk84bQdpPJwGknDJtDvbI07Azys81qTP4uDTYhmveGTqn4AyE= open-ssh-ca@cloudflareaccess.org";
+  public-keys = [
+    # Cloudflare short-lived CA
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGGucn5vE5q4Rk5ulsFVUAVSwQVbSEcwl0R7bxJk84bQdpPJwGknDJtDvbI07Azys81qTP4uDTYhmveGTqn4AyE= open-ssh-ca@cloudflareaccess.org"
+
+    # Cloudflare SSH CA (Access for Infrastructure)
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBADnXTRi0epgyH2NkIY1wNFOdcF6kX7YgKotAoPf/2fZ4nG09mPD0Z5Vr+IHmhrN8KLw6E2U41ljab7FrmPnIf0= open-ssh-ca@cloudflareaccess.org"
+  ];
 in
 
 {
@@ -41,5 +47,21 @@ in
         Restart = "on-failure";
       };
     };
-  users.users.aiotter.openssh.authorizedKeys.keys = [ public-key ];
+
+  users.users.aiotter.openssh.authorizedKeys.keys = public-keys;
+  environment.etc."ssh/ca.pub" = { mode = "600"; text = lib.concatStringsSep "\n" public-keys; };
+
+  services.openssh = {
+    settings = {
+      TrustedUserCAKeys = "/etc/ssh/ca.pub";
+      Macs = [ "hmac-sha2-256" "hmac-sha2-512" ];
+    };
+    extraConfig = ''
+      # Cloudflare Access 経由でのログインはすべて aiotter としてログインできる
+      # https://developers.cloudflare.com/cloudflare-one/applications/non-http/short-lived-certificates-legacy/
+      Match User aiotter
+        AuthorizedPrincipalsCommand ${pkgs.lib.getExe pkgs.bash} -c "echo '%t %k' | ssh-keygen -L -f - | grep -A1 Principals"
+        AuthorizedPrincipalsCommandUser nobody
+    '';
+  };
 }
