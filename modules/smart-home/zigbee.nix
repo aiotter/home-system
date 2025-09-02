6d51408185c33c4fe96404c1e3233a0897120e6c
aiotter <git@aiotter.com>
2025-09-03 01:25:00 +0900

    feat(smart-home/zigbee): zigbee2mqtt 導入

---
+++ modules/smart-home/zigbee.nix
@@ -0,0 +1,21 @@
+{
+  services = {
+    mosquitto.enable = true;
+
+    zigbee2mqtt = {
+      enable = true;
+      settings = {
+        # permit_join = true;
+        mqtt.base_topic = "zigbee2mqtt";
+        # 左下の USB ポート
+        serial.port = "/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-port0";
+        frontend = {
+          enabled = true;
+          port = 8080;
+        };
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8080 ];
+}
