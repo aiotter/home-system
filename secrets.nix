66bb75ed73e501b7ded0f83ab0d69c5919f54a67
aiotter <git@aiotter.com>
2026-05-14 19:25:59 +0900

    feat(smart-home): 再起動後も secret を保つようにする

--- secrets.nix
+++ secrets.nix
@@ -1,8 +1,15 @@
-{ pkgs, lib, config, ... }:
-
 {
   deployment.keys = {
-    cloudflared-token.keyCommand = [ "printenv" "TUNNEL_TOKEN" ];
-    # "cloudflared-credential.json".keyCommand = [ "printenv" "TUNNEL_CREDENTIAL" ];
+    cloudflared-token = {
+      keyCommand = [ "printenv" "TUNNEL_TOKEN" ];
+      destDir = "/var/keys"; # permanent
+      permissions = "0600";
+    };
+
+    # "cloudflared-credential.json" = {
+    #   keyCommand = [ "printenv" "TUNNEL_CREDENTIAL" ];
+    #   destDir = "/var/keys"; # permanent
+    #   permissions = "0600";
+    # };
   };
 }
